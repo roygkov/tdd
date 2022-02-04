75c3568f4008502daa2ee2667ac79b2e55620aa1
roygkov <rozhkov.eugene86@gmail.com>
2022-02-04 12:45:30 +0300

    Refactoring

--- src/main/java/com/example/tdd/service/BookService.java
+++ src/main/java/com/example/tdd/service/BookService.java
@@ -5,6 +5,7 @@ import com.example.tdd.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -17,6 +18,12 @@ public class BookService {
 
   public List<Book> retrieveAll() {
     return repository.findAll();
+  }
+
+  public Optional<Book> retrieveByName(final String name) {
+    return repository.findAll().stream()
+        .filter(book -> book.getName().equals(name))
+        .findAny();
   }
 
 }
--- src/test/java/com/example/tdd/service/BookServiceTest.java
+++ src/test/java/com/example/tdd/service/BookServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -20,12 +21,12 @@ class BookServiceTest {
   private BookRepository repository;
 
   @Test
-  void test_retrieveAll() {
+  void test_RetrieveAll() {
     // given
     final BookService service = new BookService(repository);
     final List<Book> allBooks = BoolUtil.getAllBooks();
 
-    when(service.retrieveAll()).thenReturn(allBooks);
+    when(repository.findAll()).thenReturn(allBooks);
 
     // when
     final List<Book> books = service.retrieveAll();
@@ -33,8 +34,24 @@ class BookServiceTest {
     // then
     assertThat(books)
         .isNotEmpty()
-        .hasSize(3)
+        .hasSize(allBooks.size())
         .containsExactlyElementsOf(allBooks);
+  }
+
+  @Test
+  void test_RetrieveByName() {
+    // given
+    final BookService service = new BookService(repository);
+    final String name = BoolUtil.getAllBooks().stream().findFirst().get().getName();
+
+    when(repository.findAll()).thenReturn(BoolUtil.getAllBooks());
+
+    // when
+    final Optional<Book> book = service.retrieveByName(name);
+
+    // then
+    assertThat(book).isNotNull();
+    assertThat(book.get().getName()).isEqualTo(name);
   }
 
 }
